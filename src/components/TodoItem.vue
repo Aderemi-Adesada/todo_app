7aa4f1e91abd07a0a5b1df066feaf2c63b953d30
aderemi-adesada <adesadaaderemi@gmail.com>
2022-01-24 16:32:13 +0100

    initial commit

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ['todo'],
+    data() {
+        return {
+            local_todo: this.todo
+        }
+    },
+    methods: {
+        markComplete() {
+            this.local_todo.completed = !this.local_todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item {
+        background: white;
+        padding: 10px;
+        border-bottom: 1px grey dotted;
+        text-align: left;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: red;
+        color: white;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
